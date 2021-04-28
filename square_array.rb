7ab601ebebc63fbfae14791e2c4733ea60bc5944
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 22:10:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
-def square_array(array)
-
-  array.each do |square_num|
+def square_array(numbers)
+  numbers.each do |square_num|
     square_num = sqaure_num**2
   end
-  return array
+  return numbers
 end
